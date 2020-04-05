c848c72533eb4806700cd60c329c04d5ee63f4d9
iQrus <39492397+iQrus@users.noreply.github.com>
2020-04-06 00:13:20 +0530

    created util class and injected into user's service constructor

--- eqarus-ms-app-ws/src/main/java/com/eqarus/app/ws/userservice/impl/UserServiceImpl.java
+++ eqarus-ms-app-ws/src/main/java/com/eqarus/app/ws/userservice/impl/UserServiceImpl.java
@@ -4,8 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eqarus.app.ws.shared.Utils;
 import com.eqarus.app.ws.ui.model.request.UserRequest;
 import com.eqarus.app.ws.ui.model.response.UserRest;
 import com.eqarus.app.ws.userservice.UserService;
@@ -14,6 +16,16 @@ import com.eqarus.app.ws.userservice.UserService;
 public class UserServiceImpl implements UserService{
 	
 	Map<String, UserRest> users;
+	Utils utils;
+	
+	public UserServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Autowired
+	public UserServiceImpl(Utils utils) {
+		this.utils = utils;
+	}
 	
 	@Override
 	public UserRest createUser(UserRequest userDetails) {
@@ -22,7 +34,7 @@ public class UserServiceImpl implements UserService{
 		returnValue.setFirstName(userDetails.getFirstName());
 		returnValue.setLastName(userDetails.getLastName());
 
-		String userId = UUID.randomUUID().toString();
+		String userId = utils.generateUserId();
 		returnValue.setUserId(userId);
 		if (users == null)
 			users = new HashMap<>();
