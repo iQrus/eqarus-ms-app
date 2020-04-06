8821b4f6460c6c047a88aaa88929d147ff5749ed
iQrus <39492397+iQrus@users.noreply.github.com>
2020-04-07 00:48:42 +0530

    spring cloud eureka discovery service code

---
+++ eqarus-discovery-service/src/test/java/com/eqarus/discovery/service/EqarusDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqarus.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqarusDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
