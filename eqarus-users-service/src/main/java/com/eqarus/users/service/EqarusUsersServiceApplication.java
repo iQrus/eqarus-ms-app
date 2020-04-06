ebf7235197cc251f120d860bc580e25a790dc14b
iQrus <39492397+iQrus@users.noreply.github.com>
2020-04-07 01:02:03 +0530

    user's microservice created, initial project

---
+++ eqarus-users-service/src/main/java/com/eqarus/users/service/EqarusUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eqarus.users.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqarusUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqarusUsersServiceApplication.class, args);
+	}
+
+}
---
+++ eqarus-users-service/src/test/java/com/eqarus/users/service/EqarusUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqarus.users.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqarusUsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
