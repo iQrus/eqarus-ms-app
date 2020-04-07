2935303c7559b44bfead29000180f89709295333
iQrus <39492397+iQrus@users.noreply.github.com>
2020-04-08 01:42:26 +0530

    configured the service to be discovery client for service registration and added vanilla controller for status check

---
+++ eqarus-account-management-service/src/main/java/com/eqarus/account/service/ui/controller/AccountController.java
@@ -0,0 +1,15 @@
+package com.eqarus.account.service.ui.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+	@GetMapping("/status/check")
+	public String status() {
+		return "account service working..";
+	}
+}
