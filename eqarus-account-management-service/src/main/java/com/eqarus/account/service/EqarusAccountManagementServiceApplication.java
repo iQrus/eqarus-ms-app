4df4d4ad4a06929382d645d5e1fb95f236e092a6
iQrus <39492397+iQrus@users.noreply.github.com>
2020-04-08 01:36:35 +0530

    user's account management microservice created

---
+++ eqarus-account-management-service/src/main/java/com/eqarus/account/service/EqarusAccountManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eqarus.account.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqarusAccountManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqarusAccountManagementServiceApplication.class, args);
+	}
+
+}
