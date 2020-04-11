e4c6e864e1142cea4cf332adf0c4aec808f62bc5
iQrus <39492397+iQrus@users.noreply.github.com>
2020-04-11 20:19:56 +0530

    adding zuul api gateway in the cluster

---
+++ eqarus-zuul-api-gateway/src/main/java/com/eqarus/api/gateway/EqarusZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.eqarus.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqarusZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqarusZuulApiGatewayApplication.class, args);
+	}
+
+}
---
+++ eqarus-zuul-api-gateway/src/test/java/com/eqarus/api/gateway/EqarusZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqarus.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqarusZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
