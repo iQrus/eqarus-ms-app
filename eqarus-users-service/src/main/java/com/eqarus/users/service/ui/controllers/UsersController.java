b3c3f9c413c6812a9b5f98859fd19f135583fe05
iQrus <39492397+iQrus@users.noreply.github.com>
2020-04-12 01:13:06 +0530

    invoked load balancer in api gateway

--- eqarus-users-service/src/main/java/com/eqarus/users/service/ui/controllers/UsersController.java
+++ eqarus-users-service/src/main/java/com/eqarus/users/service/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.eqarus.users.service.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")
 public class UsersController {
 	
+	@Autowired
+	Environment env;
+	
 	@GetMapping
 	public String status() {
-		return "working";
+		return "User service running on port : "+env.getProperty("local.server.port");
 	}
 
 }
